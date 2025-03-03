8b5551c32ef24ae985db406250da02787604df2c
AmeliaChoi <choijin9561@gmail.com>
2025-03-03 21:12:51 +0900

    desktop, taskbar 일부 수정

---
+++ src/components/desktop.js
@@ -0,0 +1,8 @@
+import React, { useState, useContext, useEffect } from 'react'
+import Notepad from './notepad';
+
+function Desktop() {
+    const data = useContext(DataContext);   
+}
+
+export default Desktop
---
+++ src/components/taskbar.js
@@ -0,0 +1,13 @@
+import React, { useContext } from 'react'
+import DataContext from '../contexts/dataContext'
+import { Taskbar, List } from '@react95/core'
+import styled from 'styled-components'
+
+const Link = styled.a`
+    text-decoration: none;
+    color: inherit;
+`
+
+function Taskbar() {
+    const { }
+}
